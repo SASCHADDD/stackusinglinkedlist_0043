180827898f977c3ab11c8bc054b20ec068bf4558
SASCHADDD <saschadanu23@gmail.com>
2024-05-11 10:22:16 +0700

    mengatur pointer next

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -26,5 +26,6 @@ class Stack {
         int push(int value){
             Node* newNode = new Node(); //1. allocation memory for the new node
             newNode->data = value; //2. assign value
+            newNode->next = top; //3. set the next pointer of the new node to the current top node
         }
 }

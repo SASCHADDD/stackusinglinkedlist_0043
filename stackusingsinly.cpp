a16bb26a52bae69d72e879df9354daf127bb1646
SASCHADDD <saschadanu23@gmail.com>
2024-05-11 10:51:35 +0700

    pilihan akan berjalan apabila tidak sampai 5

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -62,3 +62,22 @@ class Stack {
             return top == NULL; //return true if the top pointer is NULL, indicating an empty stack
     }
 };
+
+int main(){
+    Stack Stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice !=5){} {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        
+    }
+    
+}
